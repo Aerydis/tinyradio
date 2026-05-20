0ecca4ff0beaddf77906185c5aa8bd01ec8f03c0
Aerydis <aerydis700@gmail.com>
2026-05-20 14:27:54 +0900

    moved current_track_name logic from player.py to controller.py

--- controller.py
+++ controller.py
@@ -9,6 +9,8 @@ import player
 #run functions
 while True:
     current_full_time = datetime.now()
-    player.play_new_track(current_full_time)
+    current_track_name = f"tracks/{current_full_time.hour:02}.ogg"
+
+    player.play_new_track(current_track_name)
     time.sleep(timer.calculate_sleep_duration(current_full_time))
-    player.stop_current_track()
+    player.stop_current_track(current_track_name)
--- player.py
+++ player.py
@@ -3,29 +3,21 @@ import time
 
 pygame.init()
 
-current_track_name = ""
-old_track_name = ""
-
-def play_new_track(current_full_time):
-    global current_track_name
-    current_track_name = f"tracks/{current_full_time.hour:02}.ogg"
+def play_new_track(current_track_name):
     print(f"loading {current_track_name}...")
     pygame.mixer.music.load(current_track_name)
     print(f"loaded {current_track_name}!")
-    
+
     print(f"playing {current_track_name}...")
     pygame.mixer.music.play(-1)
     print(f"now playing {current_track_name}!")
 
-def stop_current_track():
-    global old_track_name
-    global current_track_name
-    old_track_name = current_track_name
-    print(f"stopping {old_track_name}...") 
+def stop_current_track(current_track_name):
+    print(f"stopping {current_track_name}...") 
     pygame.mixer.music.fadeout(5000) #fade out over 5 seconds
     time.sleep(5) #wait for fadeout to finish
-    print(f"stopped {old_track_name}!")
+    print(f"stopped {current_track_name}!")
 
-    print(f"unloading {old_track_name}...")
+    print(f"unloading {current_track_name}...")
     pygame.mixer.music.unload()
-    print(f"unloaded {old_track_name}!")
+    print(f"unloaded {current_track_name}!")
